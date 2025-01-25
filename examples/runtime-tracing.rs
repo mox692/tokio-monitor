2d03165752428e1b289a1d90e01e4ab99cb25970
mox692 <moymoymox@gmail.com>
2025-01-25 11:42:42 +0900

    ignore warning

--- examples/runtime-tracing.rs
+++ examples/runtime-tracing.rs
@@ -3,6 +3,29 @@ fn main() {
     use std::sync::atomic::{AtomicUsize, Ordering};
     use tracing_perfetto::external::tokio::TokioPerfettoLayerBuilder;
     use tracing_subscriber::prelude::*;
+
+    #[inline(never)]
+    async fn foo() {
+        bar().await
+    }
+    #[inline(never)]
+    async fn bar() {
+        baz().await
+    }
+    #[inline(never)]
+    async fn baz() {
+        let mut handles = vec![];
+        for i in 0..10000 {
+            handles.push(tokio::task::spawn(async move {
+                tokio::time::sleep(std::time::Duration::from_micros(i * 10)).await;
+            }));
+        }
+
+        for handle in handles {
+            let _ = handle.await;
+        }
+    }
+
     let layer = TokioPerfettoLayerBuilder::new()
         .file_name("./test.pftrace")
         .build();
@@ -22,28 +45,6 @@ fn main() {
     rt.block_on(async {
         tokio::spawn(async { foo().await }).await.unwrap();
     });
-}
-
-#[inline(never)]
-async fn foo() {
-    bar().await
-}
-#[inline(never)]
-async fn bar() {
-    baz().await
-}
-#[inline(never)]
-async fn baz() {
-    let mut handles = vec![];
-    for i in 0..10000 {
-        handles.push(tokio::task::spawn(async move {
-            tokio::time::sleep(std::time::Duration::from_micros(i * 10)).await;
-        }));
-    }
-
-    for handle in handles {
-        let _ = handle.await;
-    }
 }
 
 #[cfg(target_os = "windows")]
