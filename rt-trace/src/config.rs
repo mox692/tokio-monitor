2985fa4e8c614e4732db02def388daafa7df6cca
Motoyuki Kimura <moymoymox@gmail.com>
2025-07-05 19:53:11 +0900

    make flight-record fns public (#30)

--- rt-trace/src/config.rs
+++ rt-trace/src/config.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+/// Configuration for the runtime trace.
 #[derive(Default, Debug, Clone)]
 pub struct Config {
     pub(crate) consumer_thread_sleep_duration: Option<Duration>,
@@ -8,11 +9,13 @@ pub struct Config {
 
 impl Config {
     /// Create a new configuration with the default values.
+    #[allow(dead_code)]
     pub fn new() -> Self {
         Self::default()
     }
 
     /// Set the consumer thread sleep duration.
+    #[allow(dead_code)]
     pub fn consumer_thread_sleep_duration(&mut self, duration: Duration) -> &mut Self {
         self.consumer_thread_sleep_duration = Some(duration);
         self
